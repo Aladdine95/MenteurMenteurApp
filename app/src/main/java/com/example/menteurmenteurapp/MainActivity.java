aca0274a2ce68660985c7fd921faf7caf66237f7
aladd <aladdineben@outlook.com>
2021-03-11 19:29:37 +0100

    Creating a new activity for the calibration, still on progress.

--- app/src/main/java/com/example/menteurmenteurapp/MainActivity.java
+++ app/src/main/java/com/example/menteurmenteurapp/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.menteurmenteurapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button startButton;
+
+    public static final int CALIBRATION_ACTIVITY_REQUEST_CODE = 30;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent calibrationActivity = new Intent(MainActivity.this, CalibrationActivity.class);
+                startActivityForResult(calibrationActivity, CALIBRATION_ACTIVITY_REQUEST_CODE);
+            }
+        });
     }
 }
