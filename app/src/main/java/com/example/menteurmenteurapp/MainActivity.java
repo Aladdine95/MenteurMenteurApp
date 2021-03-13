c3f60a4955426c15d070104739377b6b3af1673f
aladd <aladdineben@outlook.com>
2021-03-13 15:17:58 +0100

    Credits dialog, new pattern of colors and new button and arragement to the layout

--- app/src/main/java/com/example/menteurmenteurapp/MainActivity.java
+++ app/src/main/java/com/example/menteurmenteurapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.menteurmenteurapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +11,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button startButton;
+    private Button startButton, creditsButton;
 
     public static final int CALIBRATION_ACTIVITY_REQUEST_CODE = 30;
     @Override
@@ -18,12 +20,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         startButton = findViewById(R.id.startButton);
+        creditsButton = findViewById(R.id.creditsButton);
 
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent calibrationActivity = new Intent(MainActivity.this, CalibrationActivity.class);
                 startActivityForResult(calibrationActivity, CALIBRATION_ACTIVITY_REQUEST_CODE);
+            }
+        });
+
+        creditsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(v.getContext())
+                        .setTitle("Crédits")
+                        .setMessage("Créateurs:\n\n" +
+                                "Aladdine Ben Romdhane\n" +
+                                "Quitterie Pilon\n" +
+                                "Enzo Kalinowski\n" +
+                                "Thomas Hec\n\n" +
+                                "Projet d'intégration : Objets connectés")
+
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_info)
+                        .show();
             }
         });
     }
